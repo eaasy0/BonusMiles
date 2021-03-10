94932cd074a0a77465b74444bbb1610830ad7619
Artur MiRgaleev <easy0@inbox.ru>
2021-03-10 16:04:24 +0500

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 3600;
+        int Amount = 20;
+        int BonusMiles = TicketPrice / Amount;
+
+        System.out.println(BonusMiles);
+
+
+    }
+
+}
